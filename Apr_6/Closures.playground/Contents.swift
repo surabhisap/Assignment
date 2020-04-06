ccd734e8bb8587b7e37eb988f5d12ed45bc4020e
Surabhi <surabhisap11@gmail.com>
2020-04-06 19:59:55 -0400

    Implemented stack view, inside table view

---
+++ Apr_6/Closures.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+//“a String value called person—and a return type of String, which will contain a greeting for that person:”
+
+func greet(person: String) -> String {
+    
+    let greeting = "Hi there!"
+    return greeting
+}
+
+
